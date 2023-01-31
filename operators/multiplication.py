175b41201ce673793e688965ace5729892e5869b
first-afk <111287233+first-afk@users.noreply.github.com>
2023-01-31 23:27:10 +0100

    Add files via upload
    python_class_tasks

---
+++ operators/multiplication.py
@@ -0,0 +1,4 @@
+a = int(input('enter first number: '))
+b = int(input('enter a second number: '))
+z = a * b
+print (f"the multiplication of {a} and {b} = {z}")
---
+++ operators/subtraction.py
@@ -0,0 +1,5 @@
+a = int(input('enter first number: '))
+b = int(input('enter a second number: '))
+z = a - b
+print(z)
+print (f"The difference of {a} and {b} = {z}")
