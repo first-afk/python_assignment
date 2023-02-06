54ed89bab6ebdc0830ba63d0686694b76ec8a97a
Esther Orieji <oriejiestherr@gmail.com>
2023-02-06 12:26:07 +0100

    class task

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def calculate(x, y):
+    print(x + y)
+    print(x / y)
+    print(x % y)
+    print(x * y)
+    print(x - y)
+
+def main( ):
+    x = int(input('Enter value of x: '))
+    y = int(input('Enter value of y: '))
+    calculate(x, y)
+
+
+main()
